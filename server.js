d66796b9814fbf7c22b74cabdd23638998ab68a7
epizarro98 <epizarro309@gmail.com>
2020-12-19 17:18:33 -0500

    finished hw

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const dbConfig = require('./config/db.config')
+const Flight = require('./models/flight.model')
 
 
 const db = require("./models");
@@ -17,6 +18,41 @@ db.mongoose
     console.error("Connection error", err);
     process.exit();
   });
+
+  let flight1 = new Flight({
+    from: 'CDG France',
+    to: 'JFK NY, USA',
+    airline: 'American Airlines'
+  })
+  flight1.save()
+  console.log('Flight saved', flight1)
+
+  let flight2 = new Flight({
+    from: 'Heathrow UK',
+    to: 'JFK NY, USA',
+    airline: 'British Airways'
+  })
+  flight2.save()
+  console.log('Flight saved', flight2)
+
+  let airport1 = new Airport({
+    name: 'JFK',
+    country: 'USA',
+    terminals: [],
+    opened: 1990
+  })
+
+    //new terimanil, pass all the variables in the model 
+​
+const terminal1 = new Terminal ({
+  name:"JFK",
+  capacity: "234324"
+})
+//pushing flights 1 and 2 into flights 
+terminal1.flights.push(flight1);
+terminal1.flights.push(flight2);
+terminal1.save()
+console.log("terminal saved", terminal1)
 
 const airport = new Airport({
 	name: "First Airport",
